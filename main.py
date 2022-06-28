e835ac6026e2c22d4a23e5040512cb878106fade
fauz33 <fauz33@gmail.com>
2022-06-28 16:23:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go from wordcloud
-import WordCloud, STOPWORDS import matplotlib.pyplot as plt
 
 st.title("Sentiment Analysis of Tweets about US Airlines")
 st.sidebar.title("Sentiment Analysis of Tweets")
