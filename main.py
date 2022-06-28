7cdc7b0cc9cef8601508ce80ebac7ed03a66aa50
fauz33 <fauz33@gmail.com>
2022-06-28 16:20:25 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go from wordcloud
+import WordCloud, STOPWORDS import matplotlib.pyplot as plt
+
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.sidebar.title("Sentiment Analysis of Tweets")
+st.markdown("This application is a Streamlit dashboard used "
+            "to analyze sentiments of tweets ????")
+st.sidebar.markdown("This application is a Streamlit dashboard used "
+            "to analyze sentiments of tweets ????")
